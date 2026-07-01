2358cbb23da139635dd376a1e0afda5687caa0a5
Filbert Christian W <114736135+winchfilbert@users.noreply.github.com>
2026-07-01 21:29:04 +0700

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.go
@@ -0,0 +1,35 @@
+// i still didn't finished this with hashmap method, as i think it can be finished by array
+// although it's n log n, but yea i have to train my hash map logic
+
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 {
+        return 0
+    }
+
+	sort.Ints(nums)
+
+	longest := 1
+	currentstreak := 1
+	// take 1, to directly take from the element before
+	for i := 1 ; i < len(nums); i++{
+		if check := nums[i-1] + 1; check == nums[i]{
+			currentstreak += 1
+		} else if nums[i] == nums[i-1]{
+			currentstreak += 0 // add 0, not initialize 0, because it's the same
+		} else{
+			// must check in here, or will miss by one number if the check at the top of if
+			if currentstreak > longest{
+				longest = currentstreak
+			}
+			currentstreak = 1
+		}
+	}
+
+	// CRUCIAL: Check one last time in case the longest streak hit the end of the array
+    if currentstreak > longest {
+        longest = currentstreak
+    }
+
+	return longest
+}
+
